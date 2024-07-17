2d571c4054b0d527fa79553244a0cd4daef416c2
garygaurav1313 <garygaurav13@gmail.com>
2024-07-17 15:48:26 +0530

    Working on the Another se tion

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
-      
+    <section className='flexCenter flex-col'>
+      <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+        <Image src="/camp.svg" alt='camp' width={50} height={50} />
+        <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
+            We are here for you
+        </p>
+        <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
+            <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'>Guide to Easy Path</h2>
+            <p>Oly with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. invite your friends, relatives and friends to have fun in the wilderness through the valley and reach th etop of the mountain</p>
+        </div>
+      </div>
     </section>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,8 +6,6 @@ const Hero = () => {
   return (
     <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
         <div className='hero-map' />
-            {/* { LEFT } */}
-
         <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
             <Image
                 src="/camp.svg"
@@ -34,7 +32,7 @@ const Hero = () => {
                 </div>
                 <p className='bold-16 lg:bold-20 text-blue-70'>
                     198k
-                    <span className='reguler-16 lg:regular-20 ml-1'>Excellemt</span>
+                    <span className='reguler-16 lg:regular-20 ml-1'>Excellent</span>
                 </p>
             </div>
             <div className='flex w-full gap-3 sm:flex-row'>
@@ -51,11 +49,8 @@ const Hero = () => {
                 />
             </div>
         </div>
-
-
-        <div className='relative flex flex-1 items-start'>
+        <div className='relative flex flex-1 items-start animate-bounce'>
             <div className='relative z-20 flex w-[268] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
-
                 <div className='flex flex-col'>
                     <div className='flexBetween'>
                         <p className='regular-16 text-gray-20'>Location</p>
@@ -63,18 +58,16 @@ const Hero = () => {
                     </div>
                     <p className='bold-20 text-white'>Aguas Calientes</p>
                 </div>  
-
                 <div className='flexBetween'>
                     <div className='flex flex-col'>
-                        <p className='regular-16 block textpgray-20'>Distance</p>
+                        <p className='regular-16 text-gray-20'>Distance</p>
                         <p className='bold-20 text-white'>173.28 mi</p>
                     </div>
                     <div className='flex flex-col'>
-                        <p className='regular-16 block textpgray-20'>Elevation</p>
+                        <p className='regular-16 text-gray-20'>Elevation</p>
                         <p className='bold-20 text-white'>2.040 km</p>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
