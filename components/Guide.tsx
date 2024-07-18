7cf7435a5f1aa9897964f047be72d13cc0606df6
garygaurav1313 <garygaurav13@gmail.com>
2024-07-18 17:38:04 +0530

    Working on the Guide in typescript

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,15 +4,24 @@ import React from 'react'
 const Guide = () => {
   return (
     <section className='flexCenter flex-col'>
-      <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+      <div className='padding-container max-container w-full pb-24'>
         <Image src="/camp.svg" alt='camp' width={50} height={50} />
         <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
             We are here for you
         </p>
         <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
             <h2 className='bold-40 lg:bold-64 xl:max-w-[390px]'>Guide to Easy Path</h2>
-            <p>Oly with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. invite your friends, relatives and friends to have fun in the wilderness through the valley and reach th etop of the mountain</p>
+            <p className='regular-16 text-gray-30 xl:max-w-[520px]'>Oly with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. invite your friends, relatives and friends to have fun in the wilderness through the valley and reach th etop of the mountain</p>
         </div>
+      </div>
+      <div className='flexCenter max-container relative w-full birder-2 border-blue-500'>
+        <Image 
+          src="/boat.png"
+          alt='boat'
+          width={1440}
+          height={580}
+          className='w-full boject-cover object-center 2xl:rounded-5xl'
+        />
       </div>
     </section>
   )
